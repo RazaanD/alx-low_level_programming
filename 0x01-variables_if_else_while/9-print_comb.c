c506e0ab84e3743289cb3c13346cfbbf422916e9
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-06-05 22:38:27 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,6 @@
 
 int main(void)
 {
-// 	int i = '0';
-
-// 	while (i <= '9')
-// 	{
-// 		putchar(i);
-// 		if (i != '9')
-// 		{
-// 			putchar(',');
-// 			putchar(' ');
-// 		}
-// 		i++;
-// 	}
 	
 	char word[28]="0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
 	int i = 0;
