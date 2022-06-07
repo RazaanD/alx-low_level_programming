b9525d9a4f8cca1da6deb1443f13b3533a8d1c6d
RazaanD <dawoodrazaan@gmail.com>
2022-06-07 16:37:54 -0700

    something

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,3 @@
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void){
-
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i = 0;
-	int repertition = 1;
-
-	while(repertition <= 10){
-		while (i < 26)
-		{
-			putchar(alphabet[i]);
-			i++;
-		}
-		putchar('\n');
-		repertition++;
-		i=0;
-	}
-}
-
+void print_alphabet_x10(void);
