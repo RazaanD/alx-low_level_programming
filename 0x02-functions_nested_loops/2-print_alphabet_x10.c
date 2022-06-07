f108d5d62897dc7eb75ac11fdaf9532ecaca960e
RazaanD <dawoodrazaan@gmail.com>
2022-06-07 16:27:32 -0700

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,6 +8,20 @@
  */
 int main(void)
 {
-    print_alphabet_x10();
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	int repertition = 1;
+
+	while(repertition <= 10){
+		while (i < 26)
+		{
+			putchar(alphabet[i]);
+			i++;
+		}
+		putchar('\n');
+		repertition++;
+		i=0;
+	}
+
     return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,20 @@
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void);
+void print_alphabet_x10(void){
+
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	int repertition = 1;
+
+	while(repertition <= 10){
+		while (i < 26)
+		{
+			putchar(alphabet[i]);
+			i++;
+		}
+		putchar('\n');
+		repertition++;
+		i=0;
+	}
+}
+
