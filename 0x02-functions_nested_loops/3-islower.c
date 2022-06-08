c9bfc5940b098c0705b669179a597b5e750e93e5
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-06-08 02:05:28 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,20 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * _islower - Entry point to receive chars
+ * @c: Receive the character
  *
- * Return: Always 0.
+ * Description: Return 1 if the letter is lowercase and 0 if is Uppercase.
+ * Return: 0
  */
-int main(void)
+int _islower(int c)
 {
-	int r;
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
