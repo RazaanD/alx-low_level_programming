381bf638b4476973b9e3f2d2f367d6748ba01bca
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-06-08 02:02:42 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -13,8 +13,8 @@ int main(void)
 	_putchar(r + '0');
 	r = _islower('o');
 	_putchar(r + '0');
-}	r = _islower(108);
+	r = _islower(108);
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
-	}
+}
