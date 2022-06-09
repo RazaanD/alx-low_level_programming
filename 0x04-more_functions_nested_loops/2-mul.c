ff0cbac4111c77f99a10f1f1f99c1ad576cb627a
RazaanD <dawoodrazaan@gmail.com>
2022-06-09 13:38:09 -0700

    new

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check for a digit 0 through 9
+ * @a: integer one
+ * @b: integer two
+ * Return: Integer
+ */
+
+int mul(int a, int b);
+{
+int i(a * b);
+		return (i);
+}
