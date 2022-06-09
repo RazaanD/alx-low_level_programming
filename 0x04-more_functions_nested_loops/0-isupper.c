a0d209cf5f24c1096ff9ccf33601eef00caec6dd
RazaanD <dawoodrazaan@gmail.com>
2022-06-09 12:05:25 -0700

    new

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *Returns 1 if c is uppercase
+ * Returns 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c == 'C')
+    return (1);
+    else
+ return (0);
+}
