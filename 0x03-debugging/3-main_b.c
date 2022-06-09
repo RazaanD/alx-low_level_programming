ac1d7bc65af4d4f8d034443c98aecc78a5b61306
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-06-09 02:50:31 +0200

    Update 3-main_b.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -7,21 +7,30 @@
  * Return: 0
  */
 
-int main(void)
-{
-	int month;
-	int day;
-	int year;
+int main() {
 
-	month = 2;
-	day = 29;
-	year = 2000;
+  int year;
+  cout << "Enter a year: ";
+  cin >> year;
 
-	printf("Date: %02d/%02d/%04d\n", month, day, year);
+  // leap year if perfectly divisible by 400
+  if (year % 400 == 0) {
+    cout << year << " is a leap year.";
+  }
+  // not a leap year if divisible by 100
+  // but not divisible by 400
+  else if (year % 100 == 0) {
+    cout << year << " is not a leap year.";
+  }
+  // leap year if not divisible by 100
+  // but divisible by 4
+  else if (year % 4 == 0) {
+    cout << year << " is a leap year.";
+  }
+  // all other years are not leap years
+  else {
+    cout << year << " is not a leap year.";
+  }
 
-	day = convert_day(month, day);
-
-	print_remaining_days(month, day, year);
-
-	return (0);
+  return 0;
 }
