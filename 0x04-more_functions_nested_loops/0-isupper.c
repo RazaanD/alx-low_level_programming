06dc5f02b3e557f69e32bf87c116fe9744ff4f3f
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-06-13 22:07:43 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,15 +10,8 @@
 
 int _isupper(int c)
 {
-<<<<<<< HEAD
-	if (c == 'A')
-    		return (1);
-    	else if(c == 'a')
- 		return (0);
-=======
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
->>>>>>> b6f8ce62f2b17d57233ec3e8e960495c28ab1fe8
 }
