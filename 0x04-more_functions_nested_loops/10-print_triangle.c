533a4d8c0a306731b584da7dfb6dd971d29a38b0
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-06-14 19:45:15 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,11 +4,42 @@
  * @size: print size
  * Return: Always 0.
  */
-int main (void)
+void print_triangle(int size)
+
 {
-printf("   /| \n");
-printf("  / | \n");
-printf(" /  | \n");
-printf("/___| \n");
-return 0;
+
+
+int i;
+int z;
+int d;
+int p;
+
+if (size > 0)
+{
+d = size - 1;
+for (i = 0; i < size ; i++)
+{
+for (z = d; z > 0 ; z--)
+{
+_putchar (' ');
+}
+
+for (p = 0; p <= i; p++)
+{
+_putchar (35);
+}
+
+d--;
+
+_putchar ('\n');
+
+
+}
+
+}
+
+else
+{
+_putchar ('\n');
+}
 }
