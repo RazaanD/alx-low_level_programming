d6c725f54bb24e52886bebf3c41fbb1e8db2e149
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-06-28 14:01:55 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  * uses puts function
  * returns : "Programming is like building a multilingual puzzle
  */
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
