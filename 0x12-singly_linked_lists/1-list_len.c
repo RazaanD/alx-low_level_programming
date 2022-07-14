4b1ad0b0b5158e7282c7b84226f72f25ce90cbbc
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-07-14 19:36:23 +0200

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * list_len - function that returns the number of elements in a linked list_t.
+ * @h: head of the linked list
+ * If str is NULL, print [0] (nil)
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter;
+
+	/* initialize counter to store the number of nodes */
+	counter = 0;
+	/* while n is not NULL */
+	while (h)
+	{
+		/* Move the struct */
+		h = h->next;
+		/* Increase the counter */
+		counter++;
+	}
+	return (counter);
+}
+Footer
+© 2022 GitHub, Inc.
