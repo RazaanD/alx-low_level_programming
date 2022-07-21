e4e4773dd9fe77998bd70317f236df9e578ba027
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-07-21 20:55:47 +0200

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
-#define MAIN.H
-
-#define BYTE_LENGHT 8
+#define MAIN_H
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -10,5 +8,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
