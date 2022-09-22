637f08935c0124942e93b7c62dc808017454bf4a
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-09-22 22:53:24 +0200

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,39 +1,31 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_print - Prints a hash table.
- * @ht: A pointer to the hash table to print.
- *
- * Description: Key/value pairs are printed in the order
- *              they appear in the array of the hash table.
+ * hash_table_print - a function that prints a hash table.
+ * @ht: is the hash table
+ * Return: a printed (printf) list
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
-	unsigned long int i;
-	unsigned char comma_flag = 0;
+	unsigned long int i = 0;
+	hash_node_t *list;
+	int oddeven = 0;
 
-	if (ht == NULL)
+	if (!ht)
 		return;
 
 	printf("{");
-	for (i = 0; i < ht->size; i++)
+	while (i < ht->size)
 	{
-		if (ht->array[i] != NULL)
+		list = ht->array[i];
+		while (list)
 		{
-			if (comma_flag == 1)
+			if (oddeven == 1)
 				printf(", ");
-
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				printf("'%s': '%s'", node->key, node->value);
-				node = node->next;
-				if (node != NULL)
-					printf(", ");
-			}
-			comma_flag = 1;
+			printf("'%s': '%s'", list->key, list->value);
+			oddeven = 1;
+			list = list->next;
 		}
+		i++;
 	}
 	printf("}\n");
 }
